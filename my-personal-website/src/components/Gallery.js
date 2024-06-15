fecc4fc27dd71748b37b622ade55ff024021f05b
Jojo Zhou 🍧 <yizzho@microsoft.com>
2024-06-15 01:31:21 -0400

    Submit all images to display

--- my-personal-website/src/components/Gallery.js
+++ my-personal-website/src/components/Gallery.js
@@ -1,102 +1,133 @@
 import React, { useState } from "react";
 import Section from "./Section";
-// import { motion } from "framer-motion";
-import { Gallery, Image } from "react-grid-gallery";
+import { Gallery } from "react-grid-gallery";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "../styles/Gallery.module.css";
+
+import ImgErlangshen from "../assets/images/Erlangshen.jpeg";
+import ImgFreedom from "../assets/images/Freedom.jpeg";
+import ImgGirl from "../assets/images/Girl-in-traditional-accessories.jpeg";
+import ImgSun from "../assets/images/Given-the-sun.jpeg";
+import ImgDust from "../assets/images/Into-dust.jpeg";
+import ImgKunlun from "../assets/images/Kunlun.jpeg";
+import ImgArrow from "../assets/images/Man-with-bow-and-arrow.jpeg";
+import ImgMikkelsen from "../assets/images/Mikkelsen.jpeg";
+import ImgGlasses from "../assets/images/Portrait-man-with-glasses.jpeg";
+import ImgPortrait from "../assets/images/Portrait.jpeg";
+import ImgPracticeAYG from "../assets/images/Practice.jpeg";
+import ImgRomeo from "../assets/images/Romeo-practice.jpeg";
+import ImgPope from "../assets/images/The-pope.jpeg";
+import ImgThranduil from "../assets/images/Thranduil.jpeg";
 
 export const images = [
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
+    src: ImgErlangshen,
     caption: "Artwork 2",
+    width: ImgErlangshen.width,
+    height: ImgErlangshen.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 1",
+    src: ImgFreedom,
+    caption: "Artwork 2",
+    width: ImgFreedom.width,
+    height: ImgFreedom.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 3",
+    src: ImgGirl,
+    caption: "Artwork 2",
+    width: ImgGirl.width,
+    height: ImgGirl.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 4",
+    src: ImgSun,
+    caption: "Artwork 2",
+    width: ImgSun.width,
+    height: ImgSun.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 5",
+    src: ImgDust,
+    caption: "Artwork 2",
+    width: ImgDust.width,
+    height: ImgDust.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 6",
+    src: ImgKunlun,
+    caption: "Artwork 2",
+    width: ImgKunlun.width,
+    height: ImgKunlun.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 7",
+    src: ImgArrow,
+    caption: "Artwork 2",
+    width: ImgArrow.width,
+    height: ImgArrow.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 8",
+    src: ImgMikkelsen,
+    caption: "Artwork 2",
+    width: ImgMikkelsen.width,
+    height: ImgMikkelsen.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 9",
+    src: ImgGlasses,
+    caption: "Artwork 2",
+    width: ImgGlasses.width,
+    height: ImgGlasses.height,
   },
   {
-    original: "https://via.placeholder.com/300",
-    src: "https://via.placeholder.com/300",
-    caption: "Artwork 10",
+    src: ImgPortrait,
+    caption: "Artwork 2",
+    width: ImgPortrait.width,
+    height: ImgPortrait.height,
+  },
+  {
+    src: ImgPracticeAYG,
+    caption: "Artwork 2",
+    width: ImgPracticeAYG.width,
+    height: ImgPracticeAYG.height,
+  },
+  {
+    src: ImgRomeo,
+    caption: "Artwork 2",
+    width: ImgRomeo.width,
+    height: ImgRomeo.height,
+  },
+  {
+    src: ImgPope,
+    caption: "Artwork 2",
+    width: ImgPope.width,
+    height: ImgPope.height,
+  },
+  {
+    src: ImgThranduil,
+    caption: "Artwork 2",
+    width: ImgThranduil.width,
+    height: ImgThranduil.height,
   },
 ];
-
-// const slides = images.map(({ original }) => ({
-//   src: original,
-//   // width,
-//   // height,
-// }));
 
 const ArtGallery = () => {
   const [index, setIndex] = useState(-1);
 
   const handleClick = (index, item) => setIndex(index);
 
-  const thumbswithSrc = images.map((image) => {
-    return {
-      ...image,
-      src: `https://raw.githubusercontent.com/Department-of-Decentralization/3-photos/main/thumbnails/${image.name}`,
-    };
-  });
-
-  const imagesWithSrc = images.map((image) => {
-    return {
-      src: `https://raw.githubusercontent.com/Department-of-Decentralization/3-photos/main/${image.name}`,
-    };
-  });
-
   return (
-    // <Section>
-    <>
-      <div>
+    <Section padding={300}>
+      <div className="gallery">
         <Gallery
-          images={thumbswithSrc}
+          width="60%"
+          images={images}
           onClick={handleClick}
           enableImageSelection={false}
-          rowHeight={240}
-          margin={4}
+          rowHeight={360}
+          layout="masonry"
+          enableLightbox={true}
         />
       </div>
       <Lightbox
-        slides={imagesWithSrc}
+        slides={images}
         open={index >= 0}
         index={index}
         close={() => setIndex(-1)}
@@ -121,8 +152,7 @@ const ArtGallery = () => {
           scrollToZoom: false,
         }}
       />
-    </>
-    // </Section>
+    </Section>
   );
 };
 
